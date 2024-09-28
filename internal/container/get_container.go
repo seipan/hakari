017a98d69bc6527369bda0de161bee021a31f0e5
seipan <yamasakipann0218@gmail.com>
2024-09-29 03:02:19 +0900

    feat(internal) : Add getConttainer

--- internal/container/get_container.go
+++ internal/container/get_container.go
@@ -1,1 +1,33 @@
 package container
+
+import (
+	"github.com/docker/docker/api/types/container"
+
+	"context"
+)
+
+func (c *Container) GetContainer(target string) (bool, error) {
+	if target == "" {
+		return false, nil
+	}
+
+	containers, err := c.Client.ContainerList(context.Background(), container.ListOptions{All: true})
+	if err != nil {
+		return false, err
+	}
+
+	var targetContainerID string
+	for _, container := range containers {
+		for _, name := range container.Names {
+			if name == "/"+target {
+				targetContainerID = container.ID
+				break
+			}
+		}
+	}
+
+	if targetContainerID != "" {
+		return true, nil
+	}
+	return false, nil
+}
